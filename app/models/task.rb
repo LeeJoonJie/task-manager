11b4529cef1034ec75ac7c8ba4a626f6d75cce1b
BreadPeanutButter <joonjie98@gmail.com>
2020-12-16 22:04:14 +0800

    Add validation for task fields
    To ensure that title, deadline, priority and is_completed is non-empty and valid.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+
+  enum priority: [:Low, :Moderate, :High]
+
+  validates :title,
+            presence: true
+  validates :deadline,
+            presence: true
+  validates :priority,
+            inclusion: { in: Task.priorities }
+  validates :is_completed,
+            inclusion: { in: [true, false] }
+
 end
