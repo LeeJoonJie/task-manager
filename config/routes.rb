cafe21731894d40f74b3ddce5f2b65d0873c1105
BreadPeanutButter <joonjie98@gmail.com>
2020-12-17 20:42:27 +0800

    Set-up basic files for react
    Created:
     1. pages_controller.rb which is used to control the website pages
     2. components/app.js which is the base react component of the website
     3. views/pages/index.html.erb which is the single root html view
    
     Instructions taken from:
     https://dev.to/harrymlevine/rails-with-react---one-repo---part-1-3k7o

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
+  # resources :tasks
 
-  root "tasks#index"
+  # root "tasks#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#index'
+
 end
