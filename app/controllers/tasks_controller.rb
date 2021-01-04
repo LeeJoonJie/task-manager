6605a1d3cd99b9e6d0d9aeda613f2bca94c94213
BreadPeanutButter <joonjie98@gmail.com>
2021-01-04 21:12:00 +0800

    Move database querying logic to task.rb
    Transfer database querying logic from tasks_controller.rb to task.rb to adhere to MVC pattern (task.rb is the Model).

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,39 +11,11 @@ class TasksController < ApplicationController
   # PUT /tasks
   # For sorting tasks
   def index_query
-    @sort_field = params[:sortField]
-    @sort_order = params[:sortOrder].upcase # DESC or ASC
-    @search_field = params[:searchField]
-    @search_string = params[:searchString]
+    query_params = { sort_field: params[:sortField], sort_order: params[:sortOrder].upcase,
+                     search_field: params[:searchField], search_string: params[:searchString] }
+    tasks = Task.all_query(**query_params)
 
-    @non_empty_tasks = Task.where.not("#{@sort_field}": nil)
-    @non_empty_tasks = @non_empty_tasks.where.not("#{@sort_field}": 0) if @sort_field.to_s == 'priority'
-    # if no priority field in task
-
-    @searched_tasks = case @search_field.to_s
-                      when 'all'
-                        search_tags
-                      .or(search_deadline)
-                      .or(search_description)
-                      .or(search_title)
-                      .or(search_priority)
-                      when 'tags'
-                        search_tags
-                      when 'deadline'
-                        search_deadline
-                      when 'description'
-                        search_description
-                      when 'title'
-                        search_title
-                      when 'priority'
-                        search_priority
-                      else
-                        render json: { errors: 'No such field' }, status: :unprocessable_entity
-                      end
-
-    @sorted_tasks = @searched_tasks.order("#{@sort_field} #{@sort_order}")
-
-    render json: { tasks: @sorted_tasks }
+    render json: { tasks: tasks }
   end
 
   # GET /tasks/:id
@@ -119,31 +91,4 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :description, :priority, :deadline, :progress, tags: [])
   end
 
-  def search_tags
-    @non_empty_tasks.where("array_to_string(tags, ', ') ILIKE ? ", "%#{@search_string}%")
-  end
-
-  def search_deadline
-    @non_empty_tasks.where("TO_CHAR(deadline, 'DD/MM/YYYY')  ILIKE ?",
-                           "%#{@search_string}%")
-                    .or(@non_empty_tasks.where("TO_CHAR(deadline, 'DD-MM-YYYY')  ILIKE ?",
-                                               "%#{@search_string}%"))
-                    .or(@non_empty_tasks.where("TO_CHAR(deadline, 'DD MM YYYY')  ILIKE ?",
-                                               "%#{@search_string}%"))
-  end
-
-  def search_description
-    @non_empty_tasks.where('description ILIKE ?', "%#{@search_string}%")
-  end
-
-  def search_title
-    @non_empty_tasks.where('title ILIKE ?', "%#{@search_string}%")
-  end
-
-  def search_priority
-    priorities = Task.priorities
-                     .select { |key, _value| key.to_s.downcase.include? @search_string.to_s.downcase }
-                     .values
-    @non_empty_tasks.where(priority: priorities)
-  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -10,4 +10,74 @@ class Task < ApplicationRecord
             presence: true,
             numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 
+  # Class method for querying tasks from database with the following specifications (arguments);
+  # sort_field, sort_order, search_field, search_string
+  def self.all_query(sort_field:, sort_order:, search_field:, search_string:)
+
+    # Removing tasks with empty sort_field
+    non_empty_tasks = Task.where.not("#{sort_field}": nil)
+    non_empty_tasks = non_empty_tasks.where.not("#{sort_field}": 0) if sort_field.to_s == 'priority'
+
+    # Search for tasks with matching search_string in the search_field
+    search_params = { non_empty_tasks: non_empty_tasks, search_string: search_string }
+    searched_tasks = case search_field.to_s
+                     when 'all'
+                       search_tags(**search_params)
+                     .or(search_deadline(**search_params))
+                     .or(search_description(**search_params))
+                     .or(search_title(**search_params))
+                     .or(search_priority(**search_params))
+                     when 'tags'
+                       search_tags(**search_params)
+                     when 'deadline'
+                       search_deadline(**search_params)
+                     when 'description'
+                       search_description(**search_params)
+                     when 'title'
+                       search_title(**search_params)
+                     when 'priority'
+                       search_priority(**search_params)
+                     end
+
+    # Sort tasks by sort_field in sort_order
+    searched_tasks.order("#{sort_field} #{sort_order}")
+
+  end
+
+  ############################### Private class methods ######################################################
+
+  def self.search_tags(non_empty_tasks:, search_string:)
+    non_empty_tasks.where("array_to_string(tags, ', ') ILIKE ? ", "%#{search_string}%")
+  end
+
+  def self.search_deadline(non_empty_tasks:, search_string:)
+    non_empty_tasks.where("TO_CHAR(deadline, 'DD/MM/YYYY')  ILIKE ?",
+                          "%#{search_string}%")
+                   .or(non_empty_tasks.where("TO_CHAR(deadline, 'DD-MM-YYYY')  ILIKE ?",
+                                             "%#{search_string}%"))
+                   .or(non_empty_tasks.where("TO_CHAR(deadline, 'DD MM YYYY')  ILIKE ?",
+                                             "%#{search_string}%"))
+  end
+
+  def self.search_description(non_empty_tasks:, search_string:)
+    non_empty_tasks.where('description ILIKE ?', "%#{search_string}%")
+  end
+
+  def self.search_title(non_empty_tasks:, search_string:)
+    non_empty_tasks.where('title ILIKE ?', "%#{search_string}%")
+  end
+
+  def self.search_priority(non_empty_tasks:, search_string:)
+    priorities = Task.priorities
+                     .select { |key, _value| key.to_s.downcase.include? search_string.to_s.downcase }
+                     .values
+    non_empty_tasks.where(priority: priorities)
+  end
+
+  private_class_method :search_tags,
+                       :search_deadline,
+                       :search_description,
+                       :search_title,
+                       :search_priority
+
 end
