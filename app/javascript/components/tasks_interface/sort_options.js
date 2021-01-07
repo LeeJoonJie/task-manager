356530bfb644ee282d66f035162570a83870c6cb
BreadPeanutButter <joonjie98@gmail.com>
2021-01-07 22:02:30 +0800

    Style sort_options.js
    Change color of buttons to linear gradient.

--- app/javascript/components/tasks_interface/sort_options.js
+++ app/javascript/components/tasks_interface/sort_options.js
@@ -31,13 +31,13 @@ const useStyles = makeStyles((theme) => ({
     fieldButton: {
         width: 180,
         leftMargin: 0,
-        backgroundColor: 'skyBlue'
+        background: 'linear-gradient(45deg, #21a9f3 30%, #21CBF3 90%)'
     },
     orderButtonDesc: {
-        backgroundColor: 'limeGreen'
+        background: 'linear-gradient(315deg, #f2cf07 0%, #55d284 74%)'
     },
     orderButtonAsc: {
-        backgroundColor: 'orange'
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
     }
 }))
 
